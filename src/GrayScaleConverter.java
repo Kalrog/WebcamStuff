7a2e4e4ff9d834775c6c31944c600d8f1dfc885a
FELIXakaFX <felix.kohlhas@icloud.com>
2016-12-15 20:33:13 +0100

    Did some work on greyscale.java

--- src/GrayScaleConverter.java
+++ src/GrayScaleConverter.java
@@ -1,6 +1,23 @@
+import java.awt.image.BufferedImage;
 
 public class GrayScaleConverter extends ImageModifier {
-	public BufferedImage apply() {
-	
+	public BufferedImage apply(BufferedImage org) {
+		return null;
+		BufferedImage img = new BufferedImage(org.getWidth(), org.getHeight(), BufferedImage.TYPE_INT_ARGB);
+		for(int x = 0; x < img.width; x++){
+			for(int y = 0; y < img.width; y++){
+				/* 00000000 00000000 00000000 11111111
+				*  ^Alpha   ^Red     ^Green   ^Blue
+				*/
+				int rgb = img.getRGB(x, y);
+				int alpha = (rgb >> 24) & 0x000000FF;
+				int red = (rgb >> 16) & 0x000000FF;
+				int green = (rgb >> 8 ) & 0x000000FF;
+				int blue = (rgb) & 0x000000FF;
+				int average = (red + green + blue)/3 
+				int argb = blue | green << 8 | red << 16 | alpha << 24
+				img.setRGB(x, y, argb)
+			}
+		}
 	}
 }
