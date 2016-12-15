0704f6709fc080c1703006dbe412c3f3f51aba0d
FELIXakaFX <felix.kohlhas@icloud.com>
2016-12-15 22:40:05 +0100

    Finished Kernel

--- src/Kernel.java
+++ src/Kernel.java
@@ -11,22 +11,25 @@ public class Kernel extends ImageModifier {
 		BufferedImage img = new BufferedImage(org.getWidth(), org.getHeight(), BufferedImage.TYPE_INT_ARGB);
 		for(int x = 1; x < img.getWidth()-1; x++){
 			for(int y = 1; y < img.getHeight()-1; y++){
-				int redtotal = 0;
-				int greentotal = 0;
-				int bluetotal = 0;
-				int alphacenter = org.getRGB(x, y) & 0xFF000000;
+				int total[] = new int[4];
+				/* 0 - Alpha
+				 * 1 - Red
+				 * 2 - Green
+				 * 3 - Blue
+				*/
+				total[1] = 0;
+				total[2] = 0;
+				total[3] = 0;
+				total[0] = org.getRGB(x, y) & 0xFF000000;
 				for(int xd = -1; xd <= 1; xd++){
 					for(int yd = -1; yd <= 1; yd++){
-						int rgb = org.getRGB(x, y);
-						int red = (rgb >> 16) & 0x000000FF;
-						int green = (rgb >> 8 ) & 0x000000FF;
-						int blue = (rgb) & 0x000000FF;
-						redtotal+=red*values[yd+1][xd+1];
-						greentotal+=green*values[yd+1][xd+1];
-						bluetotal+=blue*values[yd+1][xd+1];
+						int rgb[] = ColorConverter.extractRGB(org.getRGB(x+xd, y+yd));
+						total[1]+=rgb[1]*values[yd+1][xd+1];
+						total[2]+=rgb[2]*values[yd+1][xd+1];
+						total[3]+=rgb[3]*values[yd+1][xd+1];
 					}
 				}
-				int argb = redtotal | greentotal << 8 | bluetotal << 16 | alphacenter << 24;
+				int argb = ColorConverter.makeRGB(total);
 				img.setRGB(x, y, argb);
 			}
 		}
