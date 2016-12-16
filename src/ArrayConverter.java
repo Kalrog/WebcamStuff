4a938bcd1e95dfa78a6eb867b4dcd1866033ac5a
FELIXakaFX <felix.kohlhas@icloud.com>
2016-12-17 00:57:27 +0100

    Renamed ImgToArray to ArrayConverter

--- src/ArrayConverter.java
+++ src/ArrayConverter.java
@@ -1,8 +1,8 @@
 import java.awt.image.BufferedImage;
 
 
-public class ImgToArray {
-	public static int[][][] convert(BufferedImage org) {
+public class ArrayConverter {
+	public static int[][][] imgtoarray(BufferedImage org) {
 		int[][][] img = new int[3][org.getWidth()][org.getHeight()];
 		for (int x = 1; x < org.getWidth() - 1; x++) {
 			for (int y = 1; y < org.getHeight() - 1; y++) {
