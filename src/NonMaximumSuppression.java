8828c2609a22cda2579b7d6d6c3302d26cbe22b1
FELIXakaFX <felix.kohlhas@icloud.com>
2016-12-17 00:11:51 +0100

    Added ImgToArray

--- src/NonMaximumSuppression.java
+++ src/NonMaximumSuppression.java
@@ -2,33 +2,32 @@ import java.awt.image.BufferedImage;
 
 
 public class NonMaximumSuppression {
-	public BufferedImage apply(BufferedImage grad, BufferedImage stre){
-		BufferedImage img = new BufferedImage(grad.getWidth(), grad.getHeight(), BufferedImage.TYPE_INT_ARGB);
-		for (int x = 1; x < grad.getWidth() - 1; x++) {
-			for (int y = 1; y < grad.getHeight() - 1; y++) {
-				int gradient = ColorConverter.extractRGB(grad.getRGB(x, y))[1];
+	public int apply(int org[][][]){
+		int[][] img;
+		for (int x = 1; x < org[0][0].length - 1; x++) {
+			for (int y = 1; y < org[0].length - 1; y++) {
 				int nx, ny;
-				if( gradient < 32 ) {
+				if( org[1][x][y] < -45 ) {
 					nx = 1;
 					ny = 0;
-				} else if( gradient < 96) {
+				} else if( org[1][x][y] < 0) {
 					nx = -1;
 					ny = 1;
-				} else if( gradient < 160) {
+				} else if( org[1][x][y] < 45) {
 					nx = 0;
 					ny = 1;
-				} else if( gradient < 224) {
+				} else if( org[1][x][y] < 90) {
 					nx = 1;
 					ny = 1;
 				} else {
 					nx = 1;
 					ny = 0;
 				}
-				if (ColorConverter.extractRGB(stre.getRGB(x + nx, y + ny))[1] > ColorConverter.extractRGB(stre.getRGB(x, y))[1] || 
-						ColorConverter.extractRGB(stre.getRGB(x - nx, y - ny))[1] > ColorConverter.extractRGB(stre.getRGB(x, y))[1]) {
-					img.setRGB(x, y, 0xFF000000);
+				if (org[0][x + nx][y + ny] > org[0][x][y] || 
+						org[0][x - nx][y - ny] > org[0][x][y]) {
+						img[x][y] = 0;
 				} else {
-					img.setRGB(x, y, stre.getRGB(x, y));
+						img[x][y] = org[0][x][y];
 				}
 			}
 		}
