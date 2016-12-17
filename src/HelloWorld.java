daecb9d24d9c83dbe181efbe28976f332a27f36d
Jonathan Kohlhas <jonathan.kohlhas185@gmail.com>
2016-12-17 01:50:13 +0100

    Tests

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -12,45 +12,40 @@ public class HelloWorld {
 		Webcam cam = Webcam.getDefault();
 		cam.open();
 
-		Display dis = new Display(cam.getViewSize().width,
-				cam.getViewSize().height);
+		Display dis = new Display(cam.getViewSize().width, cam.getViewSize().height);
 
-		BufferedImage img, one, two, grad, stre;
+		BufferedImage img;
+
+		int[][][] sobeloutput;
+
+		int[][] imgarray;
 
 		Graphics g;
 
 		GrayScaleConverter gary = new GrayScaleConverter();
 
-		Kernel gauss = new Kernel(new int[][] { { 1, 2, 1 }, { 2, 4, 2 },
-				{ 1, 2, 1 } });
+		Kernel gauss = new Kernel(new int[][] { { 1, 2, 1 }, { 2, 4, 2 }, { 1, 2, 1 } });
 
-		Kernel gausss = new Kernel(new int[][] { { 2, 4, 5, 4, 2 },
-												 { 4, 9,12, 9, 4 }, 
-												 { 5,12,15,12, 5 }, 
-												 { 4, 9,12, 9, 4 },
-												 { 2, 4, 5, 4, 2 } });
+		Kernel gausss = new Kernel(new int[][] { { 2, 4, 5, 4, 2 }, { 4, 9, 12, 9, 4 }, { 5, 12, 15, 12, 5 },
+				{ 4, 9, 12, 9, 4 }, { 2, 4, 5, 4, 2 } });
 
-		Kernel sobelv = new Kernel(new int[][] { { 3, 0, -3 }, { 10, 0, -10 },
-				{ 3, 0, -3 } });
+		Kernel sobelv = new Kernel(new int[][] { { 3, 0, -3 }, { 10, 0, -10 }, { 3, 0, -3 } });
 
-		Kernel sobelh = new Kernel(new int[][] { { 3, 10, 3 }, { 0, 0, 0 },
-				{ -3, -10, -3 } });
+		Kernel sobelh = new Kernel(new int[][] { { 3, 10, 3 }, { 0, 0, 0 }, { -3, -10, -3 } });
 
 		Average aver = new Average();
 
-		Sobel sob = new Sobel();
+		Sobel sob = new Sobel(sobelh, sobelv);
 
 		Threshold thre = new Threshold();
 
 		Invert inv = new Invert();
-		
+
 		Mirror mir = new Mirror();
 
 		NonMaximumSuppression nms = new NonMaximumSuppression();
-		
 
 		Hysteresis hyst = new Hysteresis();
-
 
 		long time = System.currentTimeMillis();
 		int frames = 0;
@@ -78,42 +73,38 @@ public class HelloWorld {
 			if (dis.check3.isSelected() == true) {
 				img = mir.apply(img);
 			}
-			
+
 			if (dis.check4.isSelected() == true) {
 				log("Starting Gauss blur");
 				for (int i = 1; i <= dis.blur.getValue(); i++) {
-				img = gauss.apply(img);
+					img = gauss.apply(img);
 				}
 
 				log("done");
 			}
-			
+
 			if (dis.check5.isSelected() == true) {
-			log("Starting Sobel Vertical");
-			one = sobelv.apply(img);
-			log("done");
+				sobeloutput = sob.apply(img);
 
-			log("Starting Sobel Horizontal");
-			two = sobelh.apply(img);
-			log("done");
+				// img = stre;
 
-			log("Starting Sobel Gradient");
-			grad = sob.makeGradient(two, one);
-			log("done");
+				imgarray = nms.apply(sobeloutput);
 
-			log("Starting Sobel Combine");
-			stre = sob.combine(one, two);
-			log("done");
-			
-			//img = stre;
-
-			img = nms.apply(grad, stre);
-			
-			if (dis.check6.isSelected() == true) {
-			img = hyst.apply(img, dis.min.getValue(), dis.max.getValue());
+				if (dis.check6.isSelected() == true) {
+					imgarray = hyst.apply(imgarray, dis.min.getValue(), dis.max.getValue());
+					int [][][] finalimg = new int[4][imgarray.length][imgarray[0].length];
+					for(int x = 0; x < finalimg[0].length;x++){
+						for(int y = 0; y< finalimg[0][0].length; y++){
+							finalimg[0][x][y]=255;
+							finalimg[1][x][y]=(int) ColorConverter.map(imgarray[x][y],0,1000000000,0,255);
+							finalimg[2][x][y]=(int) ColorConverter.map(imgarray[x][y],0,1000000000,0,255);
+							finalimg[3][x][y]=(int) ColorConverter.map(imgarray[x][y],0,1000000000,0,255);
+						}
+					}
+					img = ArrayConverter.arraytoimg(finalimg);
+				}
 			}
-			}
-			
+
 			log("Starting Threshold");
 			// one = thre.apply(two, 10);
 			log("done");
@@ -127,12 +118,12 @@ public class HelloWorld {
 			g = dis.getBuffer().getDrawGraphics();
 
 			if (dis.check7.isSelected() == true) {
-			g.drawImage(img, 0, 0, dis.getWidth(), dis.getHeight(), null);
-			dis.addComponentListener(dis);
+				g.drawImage(img, 0, 0, dis.getWidth(), dis.getHeight(), null);
+				dis.addComponentListener(dis);
 			} else {
-			g.drawImage(img, 0, 0, null);
+				g.drawImage(img, 0, 0, null);
 			}
-			
+
 			dis.getBuffer().show();
 
 			frames++;
@@ -140,8 +131,8 @@ public class HelloWorld {
 	}
 
 	public static void log(String message) {
-		//logtime = System.currentTimeMillis() - logtime;
+		// logtime = System.currentTimeMillis() - logtime;
 		// System.out.println(logtime + " : " + message);
-		//logtime = System.currentTimeMillis();
+		// logtime = System.currentTimeMillis();
 	}
 }
